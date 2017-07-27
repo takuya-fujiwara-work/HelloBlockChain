b40534c13baea8ac26f2cff9e264e9c7ed864b94
takuya-fujiwara-work <takuya.fujiwara.work@gmail.com>
2017-07-27 16:06:34 +0900

    Update UnspentController.cs

--- HelloBlockChain/Controllers/UnspentController.cs
+++ HelloBlockChain/Controllers/UnspentController.cs
@@ -12,10 +12,10 @@ namespace HelloBlockChain.Controllers
     public class UnspentController : ApiController
     {
         [HttpGet]
-        public IEnumerable<Transaction> Get(string clientName)
+        public IEnumerable<Transaction> Get(string name)
         {
-            if (!Constants.KnownPublicKeys.ContainsKey(clientName)) { return null; }
-            var unspent = Transaction.CollectUTXO(BlocksController.blockChain, Constants.KnownPublicKeys[Constants.ClientName]);
+            if (!Constants.KnownPublicKeys.ContainsKey(name)) { return null; }
+            var unspent = Transaction.CollectUTXO(BlocksController.blockChain, Constants.KnownPublicKeys[name]);
             return unspent.Values.ToList();
         }
     }
